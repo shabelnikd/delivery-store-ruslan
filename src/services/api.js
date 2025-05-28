3c89d44ad559a8faa4e810630a5a6682e75926de
shabelnikd <shabelnikdanil.main@gmail.com>
2025-05-29 00:19:47 +0600

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://ruslan.diplomcomtehno.online/';
+const API_URL = 'https://ruslan.diplomcomtehno.online/api';
 
 // Create axios instance
 const api = axios.create({
